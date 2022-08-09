635f3e6c2f063af06e4cfcc1d359fe0d21f12d5c
Hironori Wada <hironori104@gmail.com>
2022-08-10 08:14:49 +0900

    wip

--- src/commands/info.ts
+++ src/commands/info.ts
@@ -22,6 +22,31 @@ export async function infoHandler(
     "Keybindings.json info",
     vscode.ViewColumn.Two
   );
+
+  const getR = (
+    numerator: number,
+    denominator: number,
+    shift: number
+  ): number => {
+    let r = Math.round(255 * (numerator / denominator)) + shift;
+    if (r > 255) {
+      r = 255;
+    }
+    if (r < 0) {
+      r = 0;
+    }
+    return r;
+    // return r.toString(16).padStart(2, "0");
+  };
+  const getGradi = (numerator: number, denominator: number) => {
+    const c = [
+      getR(numerator, denominator, 50), // R
+      0, // getR(numerator, denominator, 0), // G
+      getR(numerator, denominator, -150), // B
+    ];
+    return c.map((e) => e.toString(16).padStart(2, "0")).join("");
+  };
+
   // <img src="https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif" width="300" />
   const rankTable = createTable([
     ["Key", "Count"],
@@ -30,13 +55,28 @@ export async function infoHandler(
       { body: c[1].toString() },
     ]),
   ]);
-  const kbdTable = createTable([
-    ['escape', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '+', 'backspace'], // prettier-ignore
-    [" ", "tab", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]"], // prettier-ignore
-    [" ", "ctrl", "a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "'", "enter"], // prettier-ignore
-    ["shift", "`", "z", "x", "c", "v", "b", "n", "m", ",", ".", "/", "\\", "shift"], // prettier-ignore
-    [" ", " ", " ", " ", "alt", "cmd", "space", "shift", " ", " ", " ", " ", " ", ""], // prettier-ignore
-  ]);
+  const kbdTable = createTable(
+    [
+      ['escape', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '+', 'backspace'], // prettier-ignore
+      [{body: "tab", params: 'colspan="2"'}, "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]"], // prettier-ignore
+      [{body: "ctrl", params: 'colspan="2"'}, "a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "'", "enter"], // prettier-ignore
+      ["shift", "`", "z", "x", "c", "v", "b", "n", "m", ",", ".", "/", "\\", "shift"], // prettier-ignore
+      [" ", " ", " ", " ", "alt", {body: "cmd", params: 'colspan="2"'}, {body: "space", params: 'colspan="2"'}, {body: "shift", params: 'colspan="2"'}, " ", " ", " "], // prettier-ignore
+    ].map((col) =>
+      col.map((cel) => {
+        return typeof cel === "string"
+          ? { body: cel, params: `bgcolor="#${getGradi(counter[cel], 300)}"` }
+          : {
+              ...cel,
+              body: cel.body + `(${counter[cel.body]})`,
+              param: `${cel.params ?? ""} bgcolor="#${getGradi(
+                counter[cel.body],
+                300
+              )}"`,
+            };
+      })
+    )
+  );
 
   panel.webview.html = `<!DOCTYPE html>
 <html lang="en">
@@ -45,6 +85,16 @@ export async function infoHandler(
   <title>Keybindings.json info</title>
 </head>
 <body>
+${createTable(
+  Array.from({ length: 10 }).map((_, i) =>
+    Array.from({ length: 10 }).map((__, j) => {
+      const g = getGradi(i * 10 + j, 100);
+      return { body: g, params: `bgcolor="#${g}"` };
+    })
+  )
+  // .map((_, i) => getGradi(i, 100))
+  // .map((c) => ({ body: c, params: `bgcolor="#${c}"` })),
+)}
   <h1>keybindings.json Info</h1>
   <h2>Configurations</h2>
   ${kbs.length}
@@ -56,20 +106,17 @@ export async function infoHandler(
 </html>`;
 }
 
-function createTable(
-  columns: (string | { body: string; params?: string; header?: boolean })[][]
-) {
-  const toCell = (
-    body: string | { body: string; params?: string; header?: boolean }
-  ): string => {
+type Cell = string | { body: string; params?: string; header?: boolean };
+
+function createTable(columns: Cell[][]) {
+  const toCell = (body: Cell): string => {
     if (typeof body === "string") {
       return `<td align="center">${body}</td>`;
     }
-
     const tag = body.header ? "th" : "td";
-
     return `<${tag} align="center" ${body.params}>${body.body}</${tag}>`;
   };
+
   return `
 <table border="1" cellpadding="6">
     ${columns.map((c) => `<tr>${c.map(toCell).join("\n")}</tr>`).join("\n")}
